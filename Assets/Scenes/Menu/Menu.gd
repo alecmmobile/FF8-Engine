50eac4d83a1949f3b1a8fca3c5528ea50864d15c
Alec M <alecm.mobile@gmail.com>
2022-02-21 22:03:00 -0600

    Created Game  Main Structure.
    Created main structure for the game.

---
+++ Assets/Scenes/Menu/Menu.gd
@@ -0,0 +1,55 @@
+extends Control
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var panel  = $PanelContainer
+
+export var menu_commands : PoolStringArray = [
+	"Junction",
+	"Item",
+	"Magic",
+	"Status",
+	"GF",
+	"Ability",
+	"Switch",
+	"Card",
+	"Config",
+	"Tutorial",
+	"Save"
+	]
+
+# Set any of the given flags from the editor.
+export(int, FLAGS,
+"Can't Escape",
+"No Exp Gained",
+"Scripted Battle",
+"Show Timer",
+"Force Back Attack",
+"Force Surprice Attack",
+"Disable Victory Fanfar",
+"Don't Show Exp Screen"
+) var Battle_Flags = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _init():
+	
+	
+	#var Panel = panel.new()
+	print (menu_commands)
+	#for menu_command
+	
+	pass
+
+
+
